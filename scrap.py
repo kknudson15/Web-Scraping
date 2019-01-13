751c471448ba822d5e4d02082215968586c8dfe0
Kyle Knudson <kyle.knudson2015@gmail.com>
2019-01-13 14:36:39 -0600

    Extracting all info and storing in csv file

--- scrap.py
+++ scrap.py
@@ -6,46 +6,60 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-r = requests.get("https://www.pythonhow.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/")
+r=requests.get("http://www.pythonhow.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/")
+c=r.content
 
-c = r.content
-
-soup = BeautifulSoup(c,"html.parser")
-#print(soup.prettify)
-
-all = soup.find_all("div", {"class":"propertyRow"})
-price = all[0].find("h4", {"class": "propPrice"}).text.replace("\n", "").replace(" ", "")
+soup=BeautifulSoup(c,"html.parser")
+all=soup.find_all("div",{"class":"propertyRow"})
+all[0].find("h4",{"class":"propPrice"}).text.replace("\n","").replace(" ","")
+page_nr=soup.find_all("a",{"class":"Page"})[-1].text
 
 list1 = []
-for item in all: 
-    d = {}
-    d["Address"] = item.find("h4", {"class": "propPrice"}).text.replace("\n", "").replace(" ", "")
-    d["Locality"] = item.find_all("span", {"class", "propAddressCollapse"})[0].text
-    d["Price"] = item.find_all("span", {"class", "propAddressCollapse"})[1].text
-    try:
-        d["Beds"] = item.find("span", {"class", "infoBed"}).find("b").text
-    except:
-        d["Beds"] = None
-    try:
-        d["Area"] = item.find("span", {"class", "infosqFt"}).find("b").text
-    except:
-        d["Area"] = None
 
-    try:
-        d["Full Baths"] = item.find("span", {"class", "infoValueFullBath"}).find("b").text
-    except:
-       d["Full Baths"] = None
+base_url = 'https://www.pythonhow.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/t=0&s='
+for page in range(0,int(page_nr)*10,10):
+    print(base_url + str(page) + '.html')
+    r = requests.get(base_url + str(page) + '.html')
+    c = r.content
+    soup = BeautifulSoup(c, "html.parser")
+    all = soup.find_all("div", {"class": "propertyRow"})
+    for item in all: 
+        d = {}
+        try: 
+            d["Address"] = item.find_all("span", {"class", "propAddressCollapse"})[0].text
+        except:
+            d["Address"] = None
+        try:
+            d["Locality"] = item.find_all("span", {"class", "propAddressCollapse"})[1].text
+        except:
+            d["Locality"] = None
+        d["Price"] = item.find("h4", {"class": "propPrice"}).text.replace("\n", "").replace(" ", "")
+        try:
+            d["Beds"] = item.find("span", {"class", "infoBed"}).find("b").text
+        except:
+            d["Beds"] = None
+        try:
+            d["Area"] = item.find("span", {"class", "infosqFt"}).find("b").text
+        except:
+            d["Area"] = None
 
-    try:
-        d["Half Baths"] = item.find("span", {"class", "infoValueHalfBath"}).find("b").text
-    except:
-       d["Half Baths"] = None
+        try:
+            d["Full Baths"] = item.find("span", {"class", "infoValueFullBath"}).find("b").text
+        except:
+            d["Full Baths"] = None
+
+        try:
+            d["Half Baths"] = item.find("span", {"class", "infoValueHalfBath"}).find("b").text
+        except:
+            d["Half Baths"] = None
 
 
-    for column_group in item.find_all("div", {"class": "columnGroup"}):
-        for feature_group, feature_name in zip(column_group.find_all("span", {"class":"featureGroup"}), column_group.find_all("span", {"class": "featureName"})):
-            if "Lot Size" in feature_group.text:
-                d["Lot Size"] = feature_name.text
-    list1.append(d)
+        for column_group in item.find_all("div", {"class": "columnGroup"}):
+            for feature_group, feature_name in zip(column_group.find_all("span", {"class":"featureGroup"}), column_group.find_all("span", {"class": "featureName"})):
+                if "Lot Size" in feature_group.text:
+                    d["Lot Size"] = feature_name.text
+        list1.append(d)
 
 df = pd.DataFrame(list1)
+
+df.to_csv('Output.csv')
